c931960b6567751f2d884b1f06a54c0e752d1077
corinamittelholzer <mittelholzer.corina@gmail.com>
2022-06-08 19:32:00 +0200

    set up project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <footer className="text-center">
+          <a
+            href="https://github.com/corinamittelholzer/dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Corina Mittelholzer
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
