2c66e2e8a6c0f7f5a186b36cad93f7e37c720d86
corinamittelholzer <mittelholzer.corina@gmail.com>
2022-06-20 15:10:19 +0200

    create photos component and add css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
   function handlePexelsResonse(response) {
-    console.log(response);
+    console.log(response.data.photos[0].src.landscape);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -23,7 +26,7 @@ export default function Dictionary() {
     let pexelsApiKey =
       "563492ad6f917000010000015b2c825d33524a24b164943f40f17407";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `Beaer ${pexelsApiKey}` };
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResonse);
   }
 
@@ -41,6 +44,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
